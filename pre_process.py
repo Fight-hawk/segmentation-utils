89f7516e1efe8d6f229b35e170775267afefa6c7
1073196399@qq.com <kanhaipeng@xdf.cn>
2021-08-31 13:57:00 +0800

    fefact:重构代码

--- pre_process.py
+++ pre_process.py
@@ -1,133 +1,140 @@
-# -*- coding: utf-8 -*-
-
 import os
-import shutil
-import zipfile
-import tarfile
+import time
+import os.path as osp
+from random import sample
+from multiprocessing import Pool
 
-from Combined_Dataset.Training_set.Composition_code_revised import do_composite
-from Combined_Dataset.Test_set.Composition_code_revised import do_composite_test
+import cv2
+import math
+import numpy as np
+from tqdm import tqdm
 
-if __name__ == '__main__':
-    # path to provided foreground images
-    fg_path = 'data/fg/'
-    # path to provided alpha mattes
-    a_path = 'data/mask/'
-    # Path to background images (MSCOCO)
-    bg_path = 'data/bg/'
-    # Path to folder where you want the composited images to go
-    out_path = 'data/merged/'
+NUM_BGS = 10
+BG_FOLDER = r"/home/ubuntu/data/backgrounds"
+FG_FOLDER = r"/home/ubuntu/data/images"
+A_FOLDER = r"/home/ubuntu/data/alphas"
+OUT_FOLDER = r'/home/ubuntu/data/matting-dataset'
 
-    train_folder = 'data/Combined_Dataset/Training_set/'
 
-    # if not os.path.exists('Combined_Dataset'):
-    zip_file = 'data/Adobe_Deep_Matting_Dataset.zip'
-    print('Extracting {}...'.format(zip_file))
+def get_bg_files():
+    bg_files = []
+    for file in os.listdir(BG_FOLDER):
+        bg_files.append(osp.join(BG_FOLDER, file))
+    return bg_files
 
-    zip_ref = zipfile.ZipFile(zip_file, 'r')
-    zip_ref.extractall('data')
-    zip_ref.close()
 
-    if not os.path.exists(bg_path):
-        zip_file = 'data/train2014.zip'
-        print('Extracting {}...'.format(zip_file))
+BG_FILES = None#get_bg_files()
 
-        zip_ref = zipfile.ZipFile(zip_file, 'r')
-        zip_ref.extractall('data')
-        zip_ref.close()
 
-        with open(os.path.join(train_folder, 'training_bg_names.txt')) as f:
-            training_bg_names = f.read().splitlines()
+def merge_fg(fg_folders):
+    """
+    合并前景文件夹
+    :param fg_folders: list, 前景文件夹路径
+    :return: list, 前景文件路径
+    """
+    pass
 
-        os.makedirs(bg_path)
-        for bg_name in training_bg_names:
-            src_path = os.path.join('data/train2014', bg_name)
-            dest_path = os.path.join(bg_path, bg_name)
-            shutil.move(src_path, dest_path)
 
-    if not os.path.exists(fg_path):
-        os.makedirs(fg_path)
+def mkdir(directory):
+    """
+    创建目录
+    :param directory: str, 目录
+    :return: str, 目录
+    """
+    if not osp.exists(directory):
+        os.makedirs(directory)
+    return directory
 
-    for old_folder in [train_folder + 'Adobe-licensed images/fg', train_folder + 'Other/fg']:
-        fg_files = os.listdir(old_folder)
-        for fg_file in fg_files:
-            src_path = os.path.join(old_folder, fg_file)
-            dest_path = os.path.join(fg_path, fg_file)
-            shutil.move(src_path, dest_path)
 
-    if not os.path.exists(a_path):
-        os.makedirs(a_path)
+def composite4(fg, bg, a, w, h):
+    """
+    合成图片
+    :param fg: 前景图
+    :param bg: 背景图
+    :param a:  matte图
+    :param w:  图片宽度
+    :param h:  图片高度
+    :return: 合成图
+    """
+    fg = np.array(fg, np.float32)
+    bg = np.array(bg[0:h, 0:w], np.float32)
+    alpha = np.zeros((h, w, 1), np.float32)
+    alpha[:, :, 0] = a / 255.
+    comp = alpha * fg + (1 - alpha) * bg
+    comp = comp.astype(np.uint8)
+    return comp
 
-    for old_folder in [train_folder + 'Adobe-licensed images/alpha', train_folder + 'Other/alpha']:
-        a_files = os.listdir(old_folder)
-        for a_file in a_files:
-            src_path = os.path.join(old_folder, a_file)
-            dest_path = os.path.join(a_path, a_file)
-            shutil.move(src_path, dest_path)
 
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
+def process(im_name, a_path, bg_path, bg_count):
+    """
+    合成前景和背景图片并保存
+    :param bg_path: 背景图片路径
+    :param a_path: alpha图片路径
+    :param im_name: 前景图片名
+    :param bg_count: 背景图编号
+    :return:
+    """
+    im = cv2.imread(osp.join(FG_FOLDER, im_name))
+    a = cv2.imread(a_path, 0)
+    h, w = im.shape[:2]
+    bg = cv2.imread(osp.join(BG_FOLDER, bg_path))
+    bh, bw = bg.shape[:2]
+    wratio = w / bw
+    hratio = h / bh
+    ratio = wratio if wratio > hratio else hratio
+    if ratio > 1:
+        bg = cv2.resize(src=bg, dsize=(math.ceil(bw * ratio), math.ceil(bh * ratio)), interpolation=cv2.INTER_CUBIC)
+
+    if im is None or bg is None or a is None:
+        return
+
+    out = composite4(im, bg, a, w, h)
+    image_name = osp.join(OUT_FOLDER, 'image', im_name[:-4] + '_' + str(bg_count) + '.jpg')
+    alpha_name = osp.join(OUT_FOLDER, 'matte', im_name[:-4] + '_' + str(bg_count) + '.png')
+    cv2.imwrite(image_name, out)
+    cv2.imwrite(alpha_name, a)
+
+
+def process_one_fg(fg_name):
+    bg_files = sample(BG_FILES, NUM_BGS)
+    a_path = osp.join(A_FOLDER, fg_name)
+    for i, bg_name in enumerate(bg_files):
+        process(fg_name, a_path, bg_name, i + 1)
+
+
+def do_composite():
+    print('Doing composite data...')
+    fg_files = os.listdir(FG_FOLDER)
+    num_samples = len(fg_files) * NUM_BGS
+    print('num_samples: ' + str(num_samples))
+
+    start = time.time()
+
+    with Pool(processes=4) as p:
+        max_ = len(fg_files)
+        print('num_fg_files: ' + str(max_))
+        with tqdm(total=max_) as pbar:
+            for i, _ in tqdm(enumerate(p.imap_unordered(process_one_fg, fg_files))):
+                pbar.update()
+
+    end = time.time()
+    elapsed = end - start
+    print('elapsed: {} seconds'.format(elapsed))
+
+
+def poisson_blending(src, src_mask, dst, dst_height, dst_width):
+    src_height, src_width, _ = src.shape
+    dst = dst[0:dst_height, 0:dst_width]
+    center = (src_width // 2, src_height // 2)
+    blending_image = cv2.seamlessClone(src, dst, src_mask, center, cv2.MONOCHROME_TRANSFER)
+    return blending_image
+
+
+if __name__ == "__main__":
     do_composite()
+    for name in tqdm(os.listdir(FG_FOLDER)):
+        img = cv2.imread(osp.join(FG_FOLDER, name))
+        alpha = cv2.imread(osp.join(A_FOLDER, name))
+        if img is None or alpha is None:
+            print(name)
 
-    # path to provided foreground images
-    fg_test_path = 'data/fg_test/'
-    # path to provided alpha mattes
-    a_test_path = 'data/mask_test/'
-    # Path to background images (PASCAL VOC)
-    bg_test_path = 'data/bg_test/'
-    # Path to folder where you want the composited images to go
-    out_test_path = 'data/merged_test/'
-
-    # test data gen
-    test_folder = 'data/Combined_Dataset/Test_set/'
-
-    if not os.path.exists(bg_test_path):
-        os.makedirs(bg_test_path)
-
-    tar_file = 'data/VOCtrainval_14-Jul-2008.tar'
-    print('Extracting {}...'.format(tar_file))
-
-    tar = tarfile.open(tar_file)
-    tar.extractall('data')
-    tar.close()
-
-    tar_file = 'data/VOC2008test.tar'
-    print('Extracting {}...'.format(tar_file))
-
-    tar = tarfile.open(tar_file)
-    tar.extractall('data')
-    tar.close()
-
-    with open(os.path.join(test_folder, 'test_bg_names.txt')) as f:
-        test_bg_names = f.read().splitlines()
-
-    for bg_name in test_bg_names:
-        tokens = bg_name.split('_')
-        src_path = os.path.join('data/VOCdevkit/VOC2008/JPEGImages', bg_name)
-        dest_path = os.path.join(bg_test_path, bg_name)
-        shutil.move(src_path, dest_path)
-
-    if not os.path.exists(fg_test_path):
-        os.makedirs(fg_test_path)
-
-    for old_folder in [test_folder + 'Adobe-licensed images/fg']:
-        fg_files = os.listdir(old_folder)
-        for fg_file in fg_files:
-            src_path = os.path.join(old_folder, fg_file)
-            dest_path = os.path.join(fg_test_path, fg_file)
-            shutil.move(src_path, dest_path)
-
-    if not os.path.exists(a_test_path):
-        os.makedirs(a_test_path)
-
-    for old_folder in [test_folder + 'Adobe-licensed images/alpha']:
-        a_files = os.listdir(old_folder)
-        for a_file in a_files:
-            src_path = os.path.join(old_folder, a_file)
-            dest_path = os.path.join(a_test_path, a_file)
-            shutil.move(src_path, dest_path)
-
-    if not os.path.exists(out_test_path):
-        os.makedirs(out_test_path)
-
-    do_composite_test()
